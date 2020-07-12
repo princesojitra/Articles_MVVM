ed7a203b9996bb1122611e81ddff99c4469cfd57
Prince Sojitra <princehsojitra@gmail.com>
2020-07-12 16:06:21 +0530

    articl vc created and code restructured

---
+++ Articles_MVVM/Articles_MVVM/View Controllers/Article List/Controller/ArticleListVC.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleListVC.swift
+//  Articles_MVVM
+//
+//  Created by Prince Sojitra on 12/07/20.
+//  Copyright © 2020 Prince Sojitra. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
